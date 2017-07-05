4dfe931c4c0dc5307693c25e83a8bb77f2179b85
Thaddeus Madison <thaddeus.madison@gmail.com>
2017-07-05 10:44:23 -0500

    implemented a patch for windows OS file creation

--- src/firebase-multiback/FirebaseBackupper.js
+++ src/firebase-multiback/FirebaseBackupper.js
@@ -3,108 +3,112 @@ const fs = require('fs');
 const cron = require('cron');
 const moment = require('moment');
 const request = require('request');
+const execSync = require('child_process').execSync;
 
 //get, set, and static are all available in classes.
-class FirebaseBackupper{
+class FirebaseBackupper {
 
-  constructor(yaml, backupInterval){
-    this.yaml = yaml;
-    this.backupInterval = backupInterval;
-    this.runner();
-    
-  }
+    constructor(yaml, backupInterval) {
+        this.yaml = yaml;
+        this.backupInterval = backupInterval;
+        this.runner();
 
-  //the override general function
-  overrideGeneral(){
-    try{
-      this.backupInterval = this.yaml['General']['interval'];
-    }
-    catch(err){
-      //TODO: implement logger and log this information
-      console.log('No general interval set. Moving on..');
-    }
-  }
-
-  // func(firebase_endpoint, secret_key, output_name)
-  makeBackup(config, fbRef){
-    const pathPrefix = 'backups/';
-    let path = "";
-    let url = "";
-    if(Object.keys(config).length === 0){
-      console.log('Not enough arguments in '+ fbRef+"\'s config");
-      return;
     }
 
-    path = (config.hasOwnProperty('output_name'))?config['output_name']:fbRef;
-    path = pathPrefix + path;
-    url = `https://${fbRef}.firebaseio.com/.json?format=export&auth=${config['secret_key']}`;
-    request(url, (err, resp, body)=>{
-      if(this.preparePath(path)){
-        
-        //fd is the file descriptor
-        let fd = fs.openSync(path + '/' + moment().format() + '.json', 'w+');
-        fs.writeFileSync(path + '/' + moment().format() + '.json', body);
-        fs.closeSync(fd);
-      }
-    })
-
-  }
-
-  //preparePath would be "saveFile", but passing the body would be time consuming on large firebase dbs.
-  preparePath(path){
-    if(fs.existsSync(path)) return true;
-
-    let pathBuilder ='.';
-    path.split('/')
-      .forEach((v)=>{
-        pathBuilder += '/' + v;
-
-        if(!fs.existsSync(pathBuilder)){
-          fs.mkdirSync(pathBuilder);
+    //the override general function
+    overrideGeneral() {
+        try {
+            this.backupInterval = this.yaml['General']['interval'];
+        } catch (err) {
+            //TODO: implement logger and log this information
+            console.log('No general interval set. Moving on..');
         }
-      });
-
-      return true;
-  }
-
-  //the backup program's runner 
-  runner(){
-
-    if(this.yaml['General'])
-    {
-      this.overrideGeneral();
-      delete this.yaml['General'];
     }
-    
-    var hi = [];
-    for(let me in this.yaml){
 
-      //The secret_key is a required field. 
-      //If not supplied, this key should be skipped.
-      try{
-        this.yaml[me].hasOwnProperty('secret_key');
-      }catch(err){
-        console.log("The secret key is a required field. skipping config " + me + "...");
-        continue;
-      }
+    // func(firebase_endpoint, secret_key, output_name)
+    makeBackup(config, fbRef) {
+        const pathPrefix = 'backups/';
+        let path = "";
+        let url = "";
+        if (Object.keys(config).length === 0) {
+            console.log('Not enough arguments in ' + fbRef + "\'s config");
+            return;
+        }
 
-      let cron_value = (this.yaml[me].hasOwnProperty('interval'))? this.yaml[me]['interval'] : this.backupInterval;
+        path = (config.hasOwnProperty('output_name')) ? config['output_name'] : fbRef;
+        path = pathPrefix + path;
+        url = `https://${fbRef}.firebaseio.com/.json?format=export&auth=${config['secret_key']}`;
+        request(url, (err, resp, body) => {
+            if (this.preparePath(path)) {
 
-      // //run cronjobs
-      hi.push(new cron.CronJob(cron_value, ()=>{ //arrow function is important here due to usage of the this keyword
-        this.makeBackup(this.yaml[me], me);
-      },
-      null,
-      true, //true says to run the job immediately
-      null // Timezone: null tells the library to take timezone of node server
-      ));
+                let timestamp = new Date().getTime();
+                if (/^win/.test(process.platform)) { //windows patch
+                    execSync('type NUL > ' + path + '/' + timestamp + '.json');
 
+                }
+
+                fs.writeFileSync(path + '/' + timestamp + '.json', body, {
+                    flag: "w+"
+                });
+            }
+        })
 
     }
-    return hi;
-  }
 
-  get printYaml ()  {console.log(this.yaml);}
+    //preparePath would be "saveFile", but passing the body would be time consuming on large firebase dbs.
+    preparePath(path) {
+        if (fs.existsSync(path)) return true;
+
+        let pathBuilder = '.';
+        path.split('/')
+            .forEach((v) => {
+                pathBuilder += '/' + v;
+
+                if (!fs.existsSync(pathBuilder)) {
+                    fs.mkdirSync(pathBuilder);
+                }
+            });
+
+        return true;
+    }
+
+    //the backup program's runner 
+    runner() {
+
+        if (this.yaml['General']) {
+            this.overrideGeneral();
+            delete this.yaml['General'];
+        }
+
+        var hi = [];
+        for (let me in this.yaml) {
+
+            //The secret_key is a required field. 
+            //If not supplied, this key should be skipped.
+            try {
+                this.yaml[me].hasOwnProperty('secret_key');
+            } catch (err) {
+                console.log("The secret key is a required field. skipping config " + me + "...");
+                continue;
+            }
+
+            let cron_value = (this.yaml[me].hasOwnProperty('interval')) ? this.yaml[me]['interval'] : this.backupInterval;
+
+            // //run cronjobs
+            hi.push(new cron.CronJob(cron_value, () => { //arrow function is important here due to usage of the this keyword
+                    this.makeBackup(this.yaml[me], me);
+                },
+                null,
+                true, //true says to run the job immediately
+                null // Timezone: null tells the library to take timezone of node server
+            ));
+
+
+        }
+        return hi;
+    }
+
+    get printYaml() { console.log(this.yaml); }
 }
 
 module.exports = FirebaseBackupper;
--- src/firebase-multiback/index.js
+++ src/firebase-multiback/index.js
@@ -2,12 +2,12 @@ const yaml = require('js-yaml');
 const fs = require('fs');
 
 //Get document, or throw exception on error
-try{
-  const firebaseSpec = yaml.safeLoad(fs.readFileSync('./prac.yaml', 'utf-8'));
+try {
+    const firebaseSpec = yaml.safeLoad(fs.readFileSync('./prac.yaml', 'utf-8'));
 
-  const FirebaseBackupper = require('./FirebaseBackupper.js');
-  var f_instance = new FirebaseBackupper(firebaseSpec, 10);
+    const FirebaseBackupper = require('./FirebaseBackupper.js');
+    var f_instance = new FirebaseBackupper(firebaseSpec, "* * * * *");
 
-} catch(e){
-  console.log(e);
-} 
+} catch (e) {
+    console.log(e);
+}
