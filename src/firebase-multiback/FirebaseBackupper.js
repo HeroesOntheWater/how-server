64df7708d06a818179d1c1d600c7785a9f196647
Thaddeus Madison Jr <thaddeus.madison@gmail.com>
2017-07-04 22:20:53 -0500

    implemented runner method using npm cron

--- src/firebase-multiback/FirebaseBackupper.js
+++ src/firebase-multiback/FirebaseBackupper.js
@@ -1,5 +1,6 @@
 'use strict'
 const fs = require('fs');
+const cron = require('cron');
 
 //get, set, and static are all available in classes.
 class FirebaseBackupper{
@@ -24,9 +25,42 @@ class FirebaseBackupper{
 
   //the backup program's runner 
   runner(){
-    this.overrideGeneral();
-    console.log('just a test');
+
+    if(this.yaml['General'])
+    {
+      this.overrideGeneral();
+      delete this.yaml['General'];
+    }
+    
+    var hi = [];
+    for(let me in this.yaml){
+
+      //The secret_key is a required field. 
+      //If not supplied, this key should be skipped.
+      try{
+        this.yaml[me].hasOwnProperty('secret_key');
+      }catch(err){
+        console.log("The secret key is a required field. skipping config " + me + "...");
+        continue;
+      }
+
+      let temp_interval = (this.yaml[me].hasOwnProperty('interval'))? this.yaml[me]['interval'] : this.backupInterval;
+
+      //run cronjobs
+      hi.push(new cron.CronJob(temp_interval, ()=>{
+        console.log('inside cron job');
+      },
+      null,
+      true, //true says to run the job immediately
+      null // Timezone: null tells the library to take timezone of node server
+      ));
+
+
+    }
+    console.log(hi);
   }
+
+  // func(firebase_endpoint, secret_key, output_name)
 
   get printYaml ()  {console.log(this.yaml);}
 }
