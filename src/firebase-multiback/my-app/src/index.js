0fc289d9b54df00fca4aa23615e49a6210e3238e
Thaddeus Madison Jr <thaddeus.madison@gmail.com>
2017-07-17 23:24:30 -0500

    fixed router with map issue

--- src/firebase-multiback/my-app/src/index.js
+++ src/firebase-multiback/my-app/src/index.js
@@ -3,17 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
-import paths from './utils/RouthPaths.js'
+import paths from './utils/RouthPaths.js';
+import App from './App';
 
 ReactDOM.render(
   <Router>
     <switch>
     {/* leaving the default path exposed for readability. all others are resolved.*/}
-    {
-      paths.map((v) => 
-        <Route path={v.path} component={v.component} />
-      )
-    }
+    <Route path="/" component={App} />
+    {paths.map((v) => {
+        return <Route path={v.path} component={v.component} />
+      })}
     </switch>
   </Router>, document.getElementById('root'));
 registerServiceWorker();
--- src/firebase-multiback/my-app/src/utils/RouthPaths.js
+++ src/firebase-multiback/my-app/src/utils/RouthPaths.js
@@ -1,10 +1,5 @@
-import App from '../App';
 import Backup_List from '../components/backup_list';
 const paths = [
-  {
-    path: '/',
-    component: App
-  },
   {
     path: '/backups/all',
     component: Backup_List
