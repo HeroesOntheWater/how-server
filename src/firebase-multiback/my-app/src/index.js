fb72cccd6f5c709117f7177ab6f4af26727883fd
Thaddeus Madison Jr <thaddeus.madison@gmail.com>
2017-07-17 22:12:10 -0500

    bumped paths to separate utility method

--- src/firebase-multiback/my-app/src/index.js
+++ src/firebase-multiback/my-app/src/index.js
@@ -5,12 +5,15 @@ import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import App from './App';
 import Backup_List from './components/backup_list';
 import registerServiceWorker from './registerServiceWorker';
+import { paths } from './utils/RouthPaths.js'
 
+console.log(paths);
 ReactDOM.render(
   <Router>
     <switch>
-      <Route path="/" component={App} />
-      <Route path="/backups/all" component={Backup_List} />
+    {paths.map((v)=>{
+      <Route path="v.path" component={v.component}/>
+    })}
     </switch>
   </Router>, document.getElementById('root'));
 registerServiceWorker();
---
+++ src/firebase-multiback/my-app/src/utils/RouthPaths.js
@@ -0,0 +1,12 @@
+const paths = [
+  {
+    path: '/',
+    component: 'App'
+  },
+  {
+    path: '/backups/all',
+    component: 'Backup_List'
+  }
+];
+
+export {paths}
